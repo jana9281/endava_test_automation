cbcf84452828a0ad075f244b13d98d1c683eca6e
Aleksandar Zizovic <Aleksandar.Zizovic@endava.com>
2018-09-18 17:02:01 +0200

    TestDigitalPage

---
+++ src/main/java/com/endava/pages/DigitalPage.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.endava.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+/**
+ * @author Aleksandar.Zizovic
+ *
+ */
+public class DigitalPage extends BasePage{
+	
+	public By digitalButton = By.xpath("//*[@id=\"secondary-nav\"]/ul/li[1]");
+
+	/**
+	 * @param driver
+	 */
+	protected DigitalPage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public void isActive() {
+		Assert.assertTrue(driver.findElement(digitalButton).getAttribute("class").contains("active"));
+	}
+}
