15dbd70e67d1649c03337169a8eb51b4e0915cf3
Aleksandar Zizovic <aleksandar.zizovic@endava.com>
2018-09-19 10:33:03 +0200

    Ispravljen TestDigitalPage

--- src/main/java/com/endava/pages/DigitalPage.java
+++ src/main/java/com/endava/pages/DigitalPage.java
@@ -11,17 +11,17 @@ import org.testng.Assert;
  * @author Aleksandar.Zizovic
  *
  */
-public class DigitalPage extends BasePage{
-	
+public class DigitalPage extends BasePage {
+
 	public By digitalButton = By.xpath("//*[@id=\"secondary-nav\"]/ul/li[1]");
 
 	/**
-	 * @param driver
+	 * @param driver - WebDriver instance
 	 */
 	protected DigitalPage(WebDriver driver) {
 		super(driver);
 	}
-	
+
 	public void isActive() {
 		Assert.assertTrue(driver.findElement(digitalButton).getAttribute("class").contains("active"));
 	}
