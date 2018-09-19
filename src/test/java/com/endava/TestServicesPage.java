21ae21495bd124738682c2f9d5fc48d83923536a
Denis Selimovski <Denis.Selimovski@endava.com>
2018-09-19 10:18:32 +0200

    dodat testServicesPage fajl

--- src/test/java/com/endava/TestServicesPage.java
+++ src/test/java/com/endava/TestServicesPage.java
@@ -9,37 +9,31 @@ import org.testng.annotations.Test;
 
 import com.endava.pages.HomePage;
 import com.endava.pages.MenuPage;
+import com.endava.pages.ServicesPage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
- * @author jana.djordjevic@endava.com
+ * @author Denis.Selimovski
  *
  */
-public class TestHomePage {
+public class TestServicesPage {
 
 	private HomePage homePage;
 	private MenuPage menuPage;
+	private ServicesPage servicesPage;
 
 	@BeforeTest
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
 	}
 
-	/*
-	 * Test validates that home page is opened by checking if contact buttons are
-	 * visible on the page
+	/**
+	 * Open "burger" menu option Click on "Services" Validate STRATEGY menu option
+	 * is shown
 	 */
 	@Test
-	public void testHomePageIsOpened() {
-		homePage = new HomePage(new ChromeDriver());
-		homePage.open();
-		new WebDriverWait(homePage.driver, 5)
-				.until(ExpectedConditions.visibilityOfElementLocated(homePage.contactButtons));
-	}
-
-	@Test
-	public void testOpenMenu() {
+	public void testStrategyMenuIsShown() {
 		homePage = new HomePage(new ChromeDriver());
 		homePage.open();
 		new WebDriverWait(homePage.driver, 5)
@@ -47,6 +41,9 @@ public class TestHomePage {
 		menuPage = homePage.openMenu();
 		new WebDriverWait(menuPage.driver, 5)
 				.until(ExpectedConditions.visibilityOfElementLocated(menuPage.navigationList));
+
+		servicesPage = menuPage.openServices();
+		servicesPage.strategyIsShown();
 	}
 
 	@AfterMethod
