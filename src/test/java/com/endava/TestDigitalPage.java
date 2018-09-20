bff7773f881ade84e52567cf38bf36d8ea2978c7
Aleksandar Zizovic <Aleksandar.Zizovic@endava.com>
2018-09-20 16:57:13 +0200

    ParametrizeBrowserType nedovrsen

--- src/test/java/com/endava/TestDigitalPage.java
+++ src/test/java/com/endava/TestDigitalPage.java
@@ -1,11 +1,7 @@
 package com.endava;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -13,8 +9,7 @@ import org.testng.annotations.Test;
 import com.endava.pages.DigitalPage;
 import com.endava.pages.HomePage;
 import com.endava.pages.MenuPage;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.endava.util.DriverWrapper;
 
 /**
  * Open "burger" menu option Click on "Digital" Validate DIGITAL is selected in
@@ -32,26 +27,35 @@ public class TestDigitalPage {
 	@BeforeTest
 	@Parameters({ "browser" })
 	public void setUp(String browser) {
-		WebDriverManager.chromedriver().setup();
-		WebDriverManager.firefoxdriver().setup();
-		WebDriverManager.iedriver().setup();
+		DriverWrapper.setUpDriver(browser);
+//		homePage = new HomePage(DriverWrapper.createDriver(browser));
+//		homePage.open();
+	}
+
+	@BeforeMethod
+	@Parameters({ "browser" })
+	public void openBrowser(String browser) {
+		homePage = new HomePage(DriverWrapper.createDriver(browser));
+		homePage.open();
 	}
 
 	@Test
 	@Parameters({ "browser" })
 	public void testDigitalPageIsOpened(String browser) {
-		if (browser.equalsIgnoreCase("chrome"))
-			homePage = new HomePage(new ChromeDriver());
-		else if (browser.equalsIgnoreCase("firefox"))
-			homePage = new HomePage(new FirefoxDriver());
-		else if (browser.equalsIgnoreCase("ie"))
-			homePage = new HomePage(new InternetExplorerDriver());
-		homePage.open();
-		new WebDriverWait(homePage.driver, 5)
-				.until(ExpectedConditions.visibilityOfElementLocated(homePage.contactButtons));
+//		if (browser.equalsIgnoreCase("chrome"))
+//			homePage = new HomePage(new ChromeDriver());
+//		else if (browser.equalsIgnoreCase("firefox"))
+//			homePage = new HomePage(new FirefoxDriver());
+//		else if (browser.equalsIgnoreCase("ie"))
+//			homePage = new HomePage(new InternetExplorerDriver());
+		// homePage.open();
+		DriverWrapper.waitUntilElementLocated(homePage.driver, 5, homePage.contactButtons);
+//		new WebDriverWait(homePage.driver, 5)
+//				.until(ExpectedConditions.visibilityOfElementLocated(homePage.contactButtons));
 		menuPage = homePage.openMenu();
-		new WebDriverWait(menuPage.driver, 5)
-				.until(ExpectedConditions.visibilityOfElementLocated(menuPage.navigationList));
+		DriverWrapper.waitUntilElementLocated(menuPage.driver, 5, menuPage.navigationList);
+//		new WebDriverWait(menuPage.driver, 5)
+//				.until(ExpectedConditions.visibilityOfElementLocated(menuPage.navigationList));
 		digitalPage = menuPage.openDigitalPage();
 
 		digitalPage.isActive();
