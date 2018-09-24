22e9e5fda3f0ec89271bde46da40abf0b7a6a432
Nadezda Petrovic <nadezda.petrovic@endava.com>
2018-09-24 13:37:03 +0200

    korekcija sa mergovanim projektima

--- src/main/java/com/endava/pages/MenuPage.java
+++ src/main/java/com/endava/pages/MenuPage.java
@@ -12,10 +12,17 @@ import org.openqa.selenium.WebDriver;
 public class MenuPage extends BasePage {
 	private static Log log = LogFactory.getLog(ServicesPage.class);
 	public By navigationList = By.className("navigation");
+	public By successStoriesItem = By.xpath("//a[text()='Success Stories']");
 	public By servicesItem = By.xpath("//a[text()='Services']");
 
 	public MenuPage(WebDriver driver) {
 		super(driver);
+	}
+
+	public SuccessStoriesPage openSuccessStories() {
+		log.debug("Open method openSuccessStories");
+		driver.findElement(successStoriesItem).click();
+		return new SuccessStoriesPage(driver);
 	}
 
 	public ServicesPage openServices() {
