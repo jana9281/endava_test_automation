1d25410e586a2183a58b5433d8395b731863718f
Denis Selimovski <Denis.Selimovski@endava.com>
2018-09-26 13:10:54 +0200

    merge sa masterom

--- src/test/java/com/endava/TestSearchBarTypeAndClick.java
+++ src/test/java/com/endava/TestSearchBarTypeAndClick.java
@@ -1,5 +1,6 @@
 package com.endava;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -49,6 +50,7 @@ public class TestSearchBarTypeAndClick {
 		menuPage.populateField(menuPage.searchBar, "Selimovski");
 		searchPage = menuPage.openSearchPage();
 		searchPage.noResultsValidation();
+		Assert.assertEquals(searchPage.getPageTitle(), "Search Results");
 	}
 
 	@AfterMethod
